2dc861e0be37fd0dc4d9906c892201f4efb96477
Mariesnlk <44849051+Mariesnlk@users.noreply.github.com>
2020-12-01 14:44:11 +0200

    replace static block into method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,9 @@
 package com.company;
 
 import java.util.List;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -14,9 +12,10 @@ public class Main {
         RPNCalculator calculator = new RPNCalculator();
 
         List<String> expression = RPNCalculator.splitLine(line);
-        Queue<String> result = calculator.convertToRPN(expression);
+        List<String> result = calculator.convertToRPN(expression);
         System.out.println(result);
         System.out.println(calculator.RPNCalculate(result));
-
+        //A=2+M[1,j,j+3]^5
+        //B=2+f(j,j+1,j+3)^8
     }
 }
--- src/com/company/RPNCalculator.java
+++ src/com/company/RPNCalculator.java
@@ -3,14 +3,34 @@ package com.company;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import static java.util.Collections.unmodifiableMap;
 
 public class RPNCalculator {
 
-    public static final String EXPRESSION = "-?[0-9.]+|[A-Za-z]+|[-+*/()^]";
+    public static final String EXPRESSION = "-?[0-9.]+|[A-Za-z]+|[-+*/()\\[\\]^=,]";
     public static Map<String, Integer> precedence;
 
-    public double RPNCalculate(Queue<String> expressions) {
+    public static  Map<String, Integer> presLoad(Map<String, Integer> precedence){
+        Map<String, Integer> tablePrior = new HashMap<>();
+        tablePrior.put("sin", 5);
+        tablePrior.put("cos", 5);
+        tablePrior.put("tan", 5);
+        tablePrior.put("ctg", 5);
+        tablePrior.put("^", 4);
+        tablePrior.put("/", 3);
+        tablePrior.put("*", 3);
+        tablePrior.put("+", 2);
+        tablePrior.put("-", 2);
+        tablePrior.put(",", 1);
+        tablePrior.put("=", 1);
+        tablePrior.put("]", 1);
+        tablePrior.put(")", 1);
+        tablePrior.put("[", 0);
+        tablePrior.put("(", 0);
+        precedence = tablePrior;
+        return precedence;
+    }
+
+    public double RPNCalculate(List<String> expressions) {
         Stack<Double> stack = new Stack<>();
         double op1;//operand
         double op2;
@@ -71,62 +91,89 @@ public class RPNCalculator {
                     }
                 }
             }
-            System.out.println("current: " + exp + ", stack: " + stack);
-
+            System.out.println("current: " + exp + ", stack: " + stack.toString());
         }
-
         return stack.peek();
-
     }
 
-    public Queue<String> convertToRPN(List<String> exp) {
-        Queue<String> result = new LinkedList<>();
+    public List<String> convertToRPN(List<String> expr) {
+        List<String> res = new LinkedList<>();
         Stack<String> stack = new Stack<>();
-        for (String token : exp) {
-            if ("(".equals(token)) {
-                stack.push(token);
+        Map<String, Integer> pres = presLoad(precedence);
+        int counter = 0;
+        for (String symbol : expr) {
+            if ("(".equals(symbol)) {
+                counter = 2;
+//                stack.push("Fn");
+                stack.push(symbol);
                 continue;
             }
-            if (")".equals(token)) {
+            if (")".equals(symbol)) {
                 while (!"(".equals(stack.peek()))
-                    result.add(stack.pop());
+                    res.add(stack.pop());
+//                res.add("Fn");
+//                res.add(Integer.toString(counter));
+//                res.add(symbol);
                 stack.pop();
                 continue;
             }
-
-            if (precedence.containsKey(token)) {
-                while (!stack.empty() && precedence.get(token) <= precedence.get(stack.peek())) {
-                    result.add(stack.pop());
-                }
-                stack.push(token);
+            if ("[".equals(symbol)) {
+                counter = 2;
+                stack.push(symbol);
                 continue;
             }
-            result.add(token);
+            if (",".equals(symbol)) {
+                counter++;
+                while (!"[".equals(stack.peek())) {
+                    res.add(stack.pop());
+                }
+//                while (!"(".equals(stack.peek())) {
+//                    res.add(stack.pop());
+//                }
+                continue;
+            }
+            if ("]".equals(symbol)) {
+                while (!"[".equals(stack.peek())) {
+                    res.add(stack.pop());
+                }
+                res.add(Integer.toString(counter));
+                res.add(symbol);
+                stack.pop();
+                continue;
+            }
+            if (pres.containsKey(symbol)) {
+                while (!stack.empty() && pres.get(symbol) <= pres.get(stack.peek())) {
+                    res.add(stack.pop());
+                }
+                stack.push(symbol);
+                continue;
+            }
+            res.add(symbol);
         }
-
         while (!stack.isEmpty()) {
-            result.add(stack.pop());
+            res.add(stack.pop());
         }
-
-        return result;
+        return res;
     }
 
-    static {
-        Map<String, Integer> temp = new HashMap<>();
-        temp.put("sin", 5);
-        temp.put("cos", 5);
-        temp.put("tan", 5);
-        temp.put("ctg", 5);
-        temp.put("^", 4);
-        temp.put("/", 3);
-        temp.put("*", 3);
-        temp.put("+", 2);
-        temp.put("-", 2);
-        temp.put(")", 1);
-        temp.put("(", 0);
-        precedence = unmodifiableMap(temp);
-    }
-
+//    static {
+//        temp.put("sin", 5);
+//        temp.put("cos", 5);
+//        temp.put("tan", 5);
+//        temp.put("ctg", 5);
+//        temp.put("^", 4);
+//        temp.put("/", 3);
+//        temp.put("*", 3);
+//        temp.put("+", 2);
+//        temp.put("-", 2);
+//        temp.put(",", 1);
+//        temp.put("=", 1);
+//        temp.put("]", 1);
+//        temp.put(")", 1);
+//        temp.put("[", 0);
+//        temp.put("(", 0);
+//        precedence = temp;
+//    }
 
     public static List<String> splitLine(String line) {
         Pattern pattern = Pattern.compile(EXPRESSION);
